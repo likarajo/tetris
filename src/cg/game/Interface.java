6f315ee17b88c191aff72346cd4fc3082e594bb9
Rajarshi Chattopadhyay <likarajo@gmail.com>
2019-06-06 22:29:07 -0500

    updated layers

--- src/cg/game/Interface.java
+++ src/cg/game/Interface.java
@@ -28,13 +28,17 @@ public class Interface extends Frame {
 
 @SuppressWarnings("serial")
 class InterfaceCanvas extends Canvas {
-	int maxX, maxY, centerX, centerY;
-	int min_d;
-	int sqSize;
+	int maxX, maxY;
+	int centerX, centerY;
+	int cx, cy;
 	float pixelSize, rWidth = 600, rHeight = 600;
-	int level=1;
-	int lines=0;
-	int score=0;
+	int level = 1;
+	int lines = 0;
+	int score = 0;
+	int[][] mainlayer = new int[10][20];
+	Color[][] layerColor = new Color[10][20];
+	Shape current;
+	Shape next;
 
 	int iX(float x) {
 		return Math.round(centerX + x / pixelSize);
@@ -48,27 +52,78 @@ class InterfaceCanvas extends Canvas {
 		Dimension d = getSize();
 		maxX = d.width - 1;
 		maxY = d.height - 1;
-		pixelSize = Math.max(rWidth/maxX, rHeight/maxY);
+		pixelSize = Math.max(rWidth / maxX, rHeight / maxY);
 		centerX = maxX / 2;
 		centerY = maxY / 2;
-		min_d = Math.min(maxX, maxY);
-		sqSize = min_d / 10;
+	}
+
+	void drawbaselayer(Graphics g) {
+		// playing area
+		g.drawRect(iX(-250), iY(250), Math.round(250 / pixelSize), Math.round(500 / pixelSize));
+
+		// next shape area
+		g.drawRect(iX(50), iY(250), Math.round(170 / pixelSize), Math.round(100 / pixelSize));
+
+		// quit button
+		g.drawRect(iX(50), iY(-210), Math.round(100 / pixelSize), Math.round(40 / pixelSize));
+
+		// strings
+		g.setFont(new Font("Helvetica", Font.BOLD, (int) (20 / pixelSize)));
+		g.drawString("Level:                        " + level, iX(50), iY(50));
+		g.drawString("Lines:                        " + lines, iX(50), iY(0));
+		g.drawString("Score:                       " + score, iX(50), iY(-50));
+		g.drawString("QUIT", iX(75), iY(-235));
+	}
+
+	void fillmain(Graphics g) {
+		for (int x = 0; x < 10; x++) {
+			for (int y = 0; y < 20; y++) {
+				if (mainlayer[x][y] == 1) {
+					x = iX(-250 + 25 * (x + 1) - 25);
+					y = iY(-250 + 25 * (y + 1));
+					g.setColor(layerColor[x][y]);
+					g.fillRect(x + 1, y + 1, Math.round(25 / pixelSize), Math.round(25 / pixelSize));
+					g.setColor(Color.black);
+					g.drawRect(x + 1, y + 1, Math.round(25 / pixelSize), Math.round(25 / pixelSize));
+				}
+
+			}
+		}
+	}
+
+	public void fillnext(Graphics g) {
+		for (int i = 0; i < 4; ++i) {
+			int x = next.x(i);
+			int y = next.y(i);
+			x = iX(160 - 25 * 2 + 25 * x);
+			y = iY(240 - 25 + 25 * y);
+			g.setColor(Color.RED);
+			g.fillRect(x, y, Math.round(25 / pixelSize), Math.round(25 / pixelSize));
+			g.setColor(Color.BLACK);
+			g.drawRect(x, y, Math.round(25 / pixelSize), Math.round(25 / pixelSize));
+		}
+
+	}
+
+	public void fillcurrent(Graphics g) {
+		for (int i = 0; i < 4; ++i) {
+			int x = cx + current.x(i);
+			int y = cy + current.y(i);
+			x = iX(-250 + 25 * x - 25);
+			y = iY(-250 + 25 * y);
+			g.setColor(Color.GREEN);
+			g.fillRect(x + 1, y + 1, Math.round(25 / pixelSize), Math.round(25 / pixelSize));
+			g.setColor(Color.BLACK);
+			g.drawRect(x + 1, y + 1, Math.round(25 / pixelSize), Math.round(25 / pixelSize));
+		
+		}
 
 	}
 
 	public void paint(Graphics g) {
 
 		initgr();
-		
-		g.drawRect(iX(-250),iY(250),Math.round(250/pixelSize),Math.round(500/pixelSize));
-		g.drawRect(iX(50),iY(250),Math.round(170/pixelSize),Math.round(100/pixelSize));
-		g.drawRect(iX(50),iY(-210),Math.round(100/pixelSize),Math.round(40/pixelSize));
-		
-		g.setFont(new Font("Helvetica",Font.BOLD,(int)(20/pixelSize)));
-		g.drawString("Level:                        "+level, iX(50), iY(50));
-		g.drawString("Lines:                        "+lines, iX(50), iY(0));
-		g.drawString("Score:                       "+score, iX(50), iY(-50));
-		g.drawString("QUIT", iX(75), iY(-235));
+		baselayer(g);
 
 	}
 }
